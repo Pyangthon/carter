7f90284a506e6a6269b82a24cbada832656082e4
Carter <42617354+Pyangthon@users.noreply.github.com>
2020-06-10 00:14:53 +0800

    新增Socket客户端
    新增客户端代码

---
+++ Socket_Client/Frm_Client_MainWindow.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Socket_Client
+{
+    partial class Frm_Client
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_ReceiveData = new System.Windows.Forms.TextBox();
+            this.tb_SendData = new System.Windows.Forms.TextBox();
+            this.tb_Con_IP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_Con_Port = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Socket_Opt = new System.Windows.Forms.Button();
+            this.btn_SendData = new System.Windows.Forms.Button();
+            this.btn_Clear_ReceData_tb = new System.Windows.Forms.Button();
+            this.btn_Clear_SendData_tb = new System.Windows.Forms.Button();
+            this.chk_RecHex = new System.Windows.Forms.CheckBox();
+            this.chk_SendHex = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // tb_ReceiveData
+            // 
+            this.tb_ReceiveData.Location = new System.Drawing.Point(12, 12);
+            this.tb_ReceiveData.Multiline = true;
+            this.tb_ReceiveData.Name = "tb_ReceiveData";
+            this.tb_ReceiveData.Size = new System.Drawing.Size(719, 349);
+            this.tb_ReceiveData.TabIndex = 0;
+            // 
+            // tb_SendData
+            // 
+            this.tb_SendData.Location = new System.Drawing.Point(12, 398);
+            this.tb_SendData.Multiline = true;
+            this.tb_SendData.Name = "tb_SendData";
+            this.tb_SendData.Size = new System.Drawing.Size(719, 96);
+            this.tb_SendData.TabIndex = 1;
+            // 
+            // tb_Con_IP
+            // 
+            this.tb_Con_IP.Location = new System.Drawing.Point(879, 29);
+            this.tb_Con_IP.Name = "tb_Con_IP";
+            this.tb_Con_IP.Size = new System.Drawing.Size(140, 25);
+            this.tb_Con_IP.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(806, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP地址";
+            // 
+            // tb_Con_Port
+            // 
+            this.tb_Con_Port.Location = new System.Drawing.Point(879, 66);
+            this.tb_Con_Port.Name = "tb_Con_Port";
+            this.tb_Con_Port.Size = new System.Drawing.Size(140, 25);
+            this.tb_Con_Port.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(806, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "端口号";
+            // 
+            // btn_Socket_Opt
+            // 
+            this.btn_Socket_Opt.Location = new System.Drawing.Point(919, 113);
+            this.btn_Socket_Opt.Name = "btn_Socket_Opt";
+            this.btn_Socket_Opt.Size = new System.Drawing.Size(100, 29);
+            this.btn_Socket_Opt.TabIndex = 4;
+            this.btn_Socket_Opt.Text = "连接";
+            this.btn_Socket_Opt.UseVisualStyleBackColor = true;
+            this.btn_Socket_Opt.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_SendData
+            // 
+            this.btn_SendData.Location = new System.Drawing.Point(748, 462);
+            this.btn_SendData.Name = "btn_SendData";
+            this.btn_SendData.Size = new System.Drawing.Size(90, 32);
+            this.btn_SendData.TabIndex = 5;
+            this.btn_SendData.Text = "发送数据";
+            this.btn_SendData.UseVisualStyleBackColor = true;
+            this.btn_SendData.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_Clear_ReceData_tb
+            // 
+            this.btn_Clear_ReceData_tb.Location = new System.Drawing.Point(748, 329);
+            this.btn_Clear_ReceData_tb.Name = "btn_Clear_ReceData_tb";
+            this.btn_Clear_ReceData_tb.Size = new System.Drawing.Size(90, 32);
+            this.btn_Clear_ReceData_tb.TabIndex = 5;
+            this.btn_Clear_ReceData_tb.Text = "清除接收";
+            this.btn_Clear_ReceData_tb.UseVisualStyleBackColor = true;
+            this.btn_Clear_ReceData_tb.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_Clear_SendData_tb
+            // 
+            this.btn_Clear_SendData_tb.Location = new System.Drawing.Point(748, 424);
+            this.btn_Clear_SendData_tb.Name = "btn_Clear_SendData_tb";
+            this.btn_Clear_SendData_tb.Size = new System.Drawing.Size(90, 32);
+            this.btn_Clear_SendData_tb.TabIndex = 5;
+            this.btn_Clear_SendData_tb.Text = "清除发送";
+            this.btn_Clear_SendData_tb.UseVisualStyleBackColor = true;
+            this.btn_Clear_SendData_tb.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // chk_RecHex
+            // 
+            this.chk_RecHex.AutoSize = true;
+            this.chk_RecHex.Location = new System.Drawing.Point(748, 304);
+            this.chk_RecHex.Name = "chk_RecHex";
+            this.chk_RecHex.Size = new System.Drawing.Size(53, 19);
+            this.chk_RecHex.TabIndex = 6;
+            this.chk_RecHex.Text = "Hex";
+            this.chk_RecHex.UseVisualStyleBackColor = true;
+            // 
+            // chk_SendHex
+            // 
+            this.chk_SendHex.AutoSize = true;
+            this.chk_SendHex.Location = new System.Drawing.Point(748, 400);
+            this.chk_SendHex.Name = "chk_SendHex";
+            this.chk_SendHex.Size = new System.Drawing.Size(53, 19);
+            this.chk_SendHex.TabIndex = 6;
+            this.chk_SendHex.Text = "Hex";
+            this.chk_SendHex.UseVisualStyleBackColor = true;
+            // 
+            // Frm_Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 540);
+            this.Controls.Add(this.chk_SendHex);
+            this.Controls.Add(this.chk_RecHex);
+            this.Controls.Add(this.btn_Clear_SendData_tb);
+            this.Controls.Add(this.btn_Clear_ReceData_tb);
+            this.Controls.Add(this.btn_SendData);
+            this.Controls.Add(this.btn_Socket_Opt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_Con_Port);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Con_IP);
+            this.Controls.Add(this.tb_SendData);
+            this.Controls.Add(this.tb_ReceiveData);
+            this.Name = "Frm_Client";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Frm_Client_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_ReceiveData;
+        private System.Windows.Forms.TextBox tb_SendData;
+        private System.Windows.Forms.TextBox tb_Con_IP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_Con_Port;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Socket_Opt;
+        private System.Windows.Forms.Button btn_SendData;
+        private System.Windows.Forms.Button btn_Clear_ReceData_tb;
+        private System.Windows.Forms.Button btn_Clear_SendData_tb;
+        private System.Windows.Forms.CheckBox chk_RecHex;
+        private System.Windows.Forms.CheckBox chk_SendHex;
+    }
+}
+
---
+++ Socket_Client/Frm_Client_MainWindow.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Socket_Client
+{
+    public partial class Frm_Client : Form
+    {
+        private Socket tcpClientSockrt = null;      // TCP Socket服务
+        public static int TCPBufferSize = 1024;        // TCP缓存最大个数
+        public byte[] TCPBuffer = new byte[TCPBufferSize];  // TCP缓存数组
+        Encoding encoder = Encoding.Default;
+        public string getDateTime
+        {
+            get { return DateTime.Now.ToString("HH:mm:ss") + Environment.NewLine; }
+        }
+        
+        public Frm_Client()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            switch(button.Name)
+            {
+                case "btn_SendData":    // 发送数据按钮
+                    Socket_SendData();
+                    break;
+                case "btn_Socket_Opt":  // 连接操作按钮
+                    ConnectionSocket();
+                    break;
+                case "btn_Clear_ReceData_tb":   // 清除接收数据文本框
+                    tb_ReceiveData.Clear();
+                    break;
+                case "btn_Clear_SendData_tb":   // 清除发送文本框数据
+                    tb_SendData.Clear();
+                    break;
+                default:
+                    break;
+            }
+        }
+        /// <summary>
+        /// 发送数据
+        /// </summary>
+        private void Socket_SendData()
+        {
+            string str = tb_SendData.Text.Trim();       // 获取发送文本框里面的数据，不允许开头和结尾有空格
+            try
+            {
+                if(str.Length > 0)
+                {
+                    byte[] sendDataArr = encoder.GetBytes(str); // 将str转换成byte值
+                    tcpClientSockrt.BeginSend(sendDataArr, 0, sendDataArr.Length, 0, null, null);   // 发送数据
+                }
+            }
+            catch (Exception)
+            {
+
+                
+            }
+        }
+
+        /// <summary>
+        /// 连接服务器
+        /// </summary>
+        private void ConnectionSocket()
+        {
+            if(btn_Socket_Opt.Text == "连接")
+            {
+                 // ip地址和端口号输入不为空
+                 if(!string.IsNullOrEmpty(tb_Con_IP.Text) && !string.IsNullOrEmpty(tb_Con_Port.Text))
+                 {
+                    try
+                    {
+                        IPAddress ipAddress = IPAddress.Parse(tb_Con_IP.Text.Trim());   // 获取IP地址
+                        int port = int.Parse(tb_Con_Port.Text.Trim());
+
+                        tcpClientSockrt = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        // 使用BeginConnect异步接收
+                        tcpClientSockrt.BeginConnect(ipAddress, port, new AsyncCallback(ConnectedCallback), tcpClientSockrt);
+                    }
+                    catch (Exception)
+                    {
+
+                        ShowMessage("IP地址或端口号错误");
+
+                    }
+
+                 }
+                 else
+                 {
+                    ShowMessage("IP地址或端口号错误");
+                 }
+            }
+            else
+            {
+                try
+                {
+                    btn_Socket_Opt.Text = "连接";
+                    tcpClientSockrt.BeginDisconnect(false, null, null); // 断开连接
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 连接完成回调函数
+        /// </summary>
+        /// <param name="ar"></param>
+        private void ConnectedCallback(IAsyncResult ar)
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            try
+            {
+                socket.EndConnect(ar);
+                // 设置异步读取数据，接收的数据存到TCPBuffer，ReadCallback函数
+                socket.BeginReceive(TCPBuffer, 0, TCPBufferSize, 0, new AsyncCallback(ReadCallBack),  socket);
+                Invoke(new Action(()=>
+                    {
+                        ShowMessage("成功连接服务器\r\n");    // 显示连接成功数据
+                        btn_Socket_Opt.Text = "断开";
+                }));
+                
+            }
+            catch (Exception e)
+            {
+
+                ShowMessage("连接失败" + e.Message + "\r\n");        // 显示错误信息
+            }
+        }
+
+        /// <summary>
+        /// 读取数据回调函数
+        /// </summary>
+        /// <param name="ar"></param>
+        private void ReadCallBack(IAsyncResult ar)
+        {
+            int canReadLen = 0;
+            Socket socket = null;
+            try
+            {
+                socket = (Socket)ar.AsyncState;      // 获取连接的Socket
+                canReadLen = socket.EndReceive(ar);     // 结束异步读取回调，获取读取的数据个数
+            } 
+            catch (Exception e)
+            {
+
+                ShowMessage(e.Message + "\r\n");
+            }
+           
+            if(canReadLen > 0)
+            {
+                Invoke(new Action(() =>
+                {
+                    if (!chk_RecHex.Checked)
+                    {
+                        string str = encoder.GetString(TCPBuffer, 0, canReadLen);   // 根据默认转码方式转为String
+                        ShowMessage(str + "\r\n");
+                    }
+                    else
+                    {
+                        byte[] temp = new byte[canReadLen];
+                        Array.Copy(TCPBuffer, 0, temp, 0, canReadLen);
+                        ShowMessage(BytesArrayHelper.ToHexString(temp));
+                    }
+
+                }));
+                // 设置异步读取数据，接收的数据缓存到TCPBuffer中，接收完成跳转ReadCallback函数
+                socket.BeginReceive(TCPBuffer, 0, TCPBufferSize, 0, new AsyncCallback(ReadCallBack), socket);
+            }
+            else // 异常状态
+            {
+                //    Invoke(new Action(() =>
+                //    {
+                //        btn_Socket_Opt.Text = "连接";
+                //        ShowMessage("异常断开\r\n");
+                //    }));
+                //    try
+                //    {
+                //        tcpClientSockrt.BeginDisconnect(false, null, null);
+                //    }
+                //    catch (Exception)
+                //    {
+
+
+            //}
+            }
+            
+        }
+
+        /// <summary>
+        /// 显示数据
+        /// </summary>
+        /// <param name="info"></param>
+        private void ShowMessage(string info)
+        {
+            Invoke(new Action(()=>
+            {
+                tb_ReceiveData.AppendText(getDateTime + info);
+            }));
+        }
+        /// <summary>
+        /// 窗口加载事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Frm_Client_Load(object sender, EventArgs e)
+        {
+            tb_Con_IP.Text = "127.0.0.1";
+            tb_Con_Port.Text = "8080";
+        }
+    }
+}
